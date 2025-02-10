325aac30744ac4d4be3dfe075d72891bf1e5a48b
Özkan Bölükbaş <ozkanbolukbas@gmail.com>
2025-02-10 20:47:58 +0300

    Changed navigation bar tint color

--- OBBilyoner/SupportFiles/AppDelegate.swift
+++ OBBilyoner/SupportFiles/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Change navigation bar tint color.
+		UINavigationBar.appearance().tintColor = .iconPrimary
 		// Configure Firebase
 		FirebaseApp.configure()
 		return true
